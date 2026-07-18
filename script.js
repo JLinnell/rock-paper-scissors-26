20156910b3cbb4501c70a5921d598eb4d4a90bca
JLinnell <linnelljudith@gmail.com>
2026-07-18 13:16:16 -0400

    Implement initial logic

--- script.js
+++ script.js
@@ -1,41 +1,84 @@
-
+let userScore = 0;
+let compScore = 0;
 
 function getComputerChoice() {
     const num = Math.floor(Math.random() * 3) + 1;
     if (num == 1) {
-        console.log('the computer chose rock');
         compChoice = 'rock';
     } else if (num == 2) {
-        console.log('the computer chose scissors');
         compChoice = 'scissors';
     } else {
-        console.log('the computer chose paper');
         compChoice = 'paper';
     }
-    console.log(`return ${compChoice}`);
     return compChoice;
     
 }
 
-getComputerChoice();
 
 function getUserChoice() {
     let userChoice = prompt("Please enter your choice:")?.toLocaleLowerCase();
     if (userChoice === 'rock') {
-        console.log('you chose rock');
         userChoice = 'rock';
     } else if (userChoice === 'scissors') {
-        console.log('you chose scissors');
         userChoice = 'scissors';
     } else if (userChoice === 'paper') {
-        console.log('you chose paper');
         userChoice = 'paper';
     } else {
         console.log('That is not a valid choice, please try again');
-        getUserChoice();
     }
+    return userChoice;
 
 }
 
-getUserChoice();
+
+function playRound (compChoice, userChoice) {
+        if (compChoice == userChoice) {
+           console.log(`The computer chose ${compChoice} and you chose ${userChoice}. It's a tie!`);
+        } else if (compChoice == 'rock' && userChoice == 'paper' || compChoice == 'paper' && userChoice == 'scissors' || compChoice == 'scissors' && userChoice == 'rock') {
+           let userRoundWins = true;
+           let compRoundWins = false;
+           console.log(`The computer chose ${compChoice} and you chose ${userChoice}. You win!`);
+           return userRoundWins;
+        } else {
+          let compRoundWins =  true;
+          let userRoundWins = false;
+          console.log(`The computer chose ${compChoice} and you chose ${userChoice}. The computer wins.`);
+          return compRoundWins;
+        } 
+
+}
+
+function getCurrentScore(userRoundWins, compRoundWins) {
+    if (userRoundWins) {
+       let userScore =+ 1;
+    } else if (compRoundWins) {
+        let userScore =+ 1;
+    } else {
+        console.log('The tie doesn\'t change the score.')
+    }
+    return {userScore, compScore};
+}
+
+function playGame () {
+    for( let round = 0; round <= 6; round++) {
+        playRound(getComputerChoice(), getUserChoice());
+        getCurrentScore();
+        round++;
+    }
+}
+
+
+function tallyScore (score) {
+    if (userScore == compScore) {
+        score = console.log (`Your score is ${userScore} and the computer's score is ${compScore}. It's a tie!`)
+    } else if (userScore > compScore) {
+        score = console.log (`Your score is ${userScore} and the computer's score is ${compScore}. You win!`)
+
+    } else {
+        score = console.log (`Your score is ${userScore} and the computer's score is ${compScore}. The computer wins.`)
+
+    }
+}
+
+tallyScore(playGame());
 
