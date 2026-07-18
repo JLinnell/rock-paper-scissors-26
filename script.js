2e9bfc4c8dedd6a3a6d30dcbb0df896e1cf2527b
JLinnell <linnelljudith@gmail.com>
2026-07-18 14:36:09 -0400

    Write functions for scoring

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let userScore = 0;
 let compScore = 0;
 
+
 function getComputerChoice() {
     const num = Math.floor(Math.random() * 3) + 1;
     if (num == 1) {
@@ -32,37 +33,23 @@ function getUserChoice() {
 
 
 function playRound (compChoice, userChoice) {
-        if (compChoice == userChoice) {
-           console.log(`The computer chose ${compChoice} and you chose ${userChoice}. It's a tie!`);
-        } else if (compChoice == 'rock' && userChoice == 'paper' || compChoice == 'paper' && userChoice == 'scissors' || compChoice == 'scissors' && userChoice == 'rock') {
-           let userRoundWins = true;
-           let compRoundWins = false;
-           console.log(`The computer chose ${compChoice} and you chose ${userChoice}. You win!`);
-           return userRoundWins;
-        } else {
-          let compRoundWins =  true;
-          let userRoundWins = false;
-          console.log(`The computer chose ${compChoice} and you chose ${userChoice}. The computer wins.`);
-          return compRoundWins;
-        } 
-
-}
-
-function getCurrentScore(userRoundWins, compRoundWins) {
-    if (userRoundWins) {
-       let userScore =+ 1;
-    } else if (compRoundWins) {
-        let userScore =+ 1;
+    if (compChoice == userChoice) {
+        console.log(`The computer chose ${compChoice} and you chose ${userChoice}. It's a tie!`);
+    } else if (compChoice == 'rock' && userChoice == 'paper' || compChoice == 'paper' && userChoice == 'scissors' || compChoice == 'scissors' && userChoice == 'rock') {
+        console.log(`The computer chose ${compChoice} and you chose ${userChoice}. You win!`);
+        userScore++;
     } else {
-        console.log('The tie doesn\'t change the score.')
-    }
-    return {userScore, compScore};
+        console.log(`The computer chose ${compChoice} and you chose ${userChoice}. The computer wins.`);
+        compScore++;
+    } 
+    return{userScore, compScore};
 }
+
 
 function playGame () {
     for( let round = 0; round <= 6; round++) {
         playRound(getComputerChoice(), getUserChoice());
-        getCurrentScore();
+        console.log(`Your current score: ${userScore} Computer's current score: ${compScore}`)
         round++;
     }
 }
@@ -70,12 +57,12 @@ function playGame () {
 
 function tallyScore (score) {
     if (userScore == compScore) {
-        score = console.log (`Your score is ${userScore} and the computer's score is ${compScore}. It's a tie!`)
+        score = console.log (`Your total score is ${userScore} and the computer's total score is ${compScore}. It's a tie!`);
     } else if (userScore > compScore) {
-        score = console.log (`Your score is ${userScore} and the computer's score is ${compScore}. You win!`)
+        score = console.log (`Your total score is ${userScore} and the computer's total score is ${compScore}. You win the whole game!!!`);
 
     } else {
-        score = console.log (`Your score is ${userScore} and the computer's score is ${compScore}. The computer wins.`)
+        score = console.log (`Your total score is ${userScore} and the computer's total score is ${compScore}. The computer wins :(`);
 
     }
 }
