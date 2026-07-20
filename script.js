49f7025cf1743b6ae28be0d0e0d6cc7fda59f0d4
JLinnell <linnelljudith@gmail.com>
2026-07-20 15:22:53 -0400

    Add event listerner for playRound function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userScore = 0;
 let compScore = 0;
+const buttons = document.querySelectorAll('.button-choice');
 
 
 function getComputerChoice() {
@@ -14,45 +15,32 @@ function getComputerChoice() {
     return compChoice;
     
 }
+let round = 0;
 
-
-function getUserChoice() {
-    let userChoice = prompt("Please enter your choice:")?.toLocaleLowerCase();
-    if (userChoice === 'rock') {
-        userChoice = 'rock';
-    } else if (userChoice === 'scissors') {
-        userChoice = 'scissors';
-    } else if (userChoice === 'paper') {
-        userChoice = 'paper';
-    } else {
-        console.log('That is not a valid choice, please try again');
-    }
-    return userChoice;
-
-}
-
-
-function playRound (compChoice, userChoice) {
-    if (compChoice == userChoice) {
-        console.log(`The computer chose ${compChoice} and you chose ${userChoice}. It's a tie!`);
-    } else if (compChoice == 'rock' && userChoice == 'paper' || compChoice == 'paper' && userChoice == 'scissors' || compChoice == 'scissors' && userChoice == 'rock') {
-        console.log(`The computer chose ${compChoice} and you chose ${userChoice}. You win!`);
-        userScore++;
-    } else {
-        console.log(`The computer chose ${compChoice} and you chose ${userChoice}. The computer wins.`);
-        compScore++;
-    } 
-    return{userScore, compScore};
-}
-
-
-function playGame () {
-    for( let round = 0; round <= 6; round++) {
-        playRound(getComputerChoice(), getUserChoice());
+ buttons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    getComputerChoice();
+    const buttonId = event.target.id;
+    while (round < 5) {
+        if (compChoice == buttonId) {
+            console.log(`The computer chose ${compChoice} and you chose ${buttonId}. It's a tie!`);
+            round++;
+        } else if (compChoice == 'rock' && buttonId == 'paper' || compChoice == 'paper' && buttonId == 'scissors' || compChoice == 'scissors' && buttonId == 'rock') {
+            console.log(`The computer chose ${compChoice} and you chose ${buttonId}. You win!`);
+            userScore++;
+            round++;
+        } else {
+            console.log(`The computer chose ${compChoice} and you chose ${buttonId}. The computer wins.`);
+            compScore++;
+            round++;
+        } 
         console.log(`Your current score: ${userScore} Computer's current score: ${compScore}`)
-        round++;
+        return{userScore, compScore};
     }
-}
+    tallyScore();
+})
+})
+
 
 
 function tallyScore (score) {
@@ -67,5 +55,7 @@ function tallyScore (score) {
     }
 }
 
-tallyScore(playGame());
+
+
+
 
