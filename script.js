b31d25dd63d5dc05ef030688a2a38e66ade91e62
JLinnell <linnelljudith@gmail.com>
2026-07-21 13:40:34 -0400

    Add initial CSS

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let userScore = 0;
 let compScore = 0;
+let round = 0;
+
 const buttons = document.querySelectorAll('.button-choice');
+const resultOutput = document.getElementById('resultText');
+const currentScoreOutput = document.getElementById('scoreText');
+const endScore = document.getElementById('totalScoreText');
 
 
 function getComputerChoice() {
@@ -15,7 +20,6 @@ function getComputerChoice() {
     return compChoice;
     
 }
-let round = 0;
 
  buttons.forEach(button => {
   button.addEventListener('click', (event) => {
@@ -23,18 +27,18 @@ let round = 0;
     const buttonId = event.target.id;
     while (round < 5) {
         if (compChoice == buttonId) {
-            console.log(`The computer chose ${compChoice} and you chose ${buttonId}. It's a tie!`);
+            resultOutput.textContent = `The computer chose ${compChoice} and you chose ${buttonId}. It's a tie!`;
             round++;
         } else if (compChoice == 'rock' && buttonId == 'paper' || compChoice == 'paper' && buttonId == 'scissors' || compChoice == 'scissors' && buttonId == 'rock') {
-            console.log(`The computer chose ${compChoice} and you chose ${buttonId}. You win!`);
+            resultOutput.textContent = `The computer chose ${compChoice} and you chose ${buttonId}. You win!`;
             userScore++;
             round++;
         } else {
-            console.log(`The computer chose ${compChoice} and you chose ${buttonId}. The computer wins.`);
+            resultOutput.textContent = `The computer chose ${compChoice} and you chose ${buttonId}. The computer wins.`;
             compScore++;
             round++;
         } 
-        console.log(`Your current score: ${userScore} Computer's current score: ${compScore}`)
+        currentScoreOutput.textContent = `Your current score: ${userScore} Computer's current score: ${compScore}`;
         return{userScore, compScore};
     }
     tallyScore();
@@ -43,15 +47,13 @@ let round = 0;
 
 
 
-function tallyScore (score) {
+function tallyScore () {
     if (userScore == compScore) {
-        score = console.log (`Your total score is ${userScore} and the computer's total score is ${compScore}. It's a tie!`);
+        resultOutput.textContent = `Your total score is ${userScore} and the computer's total score is ${compScore}. It's a tie!`;
     } else if (userScore > compScore) {
-        score = console.log (`Your total score is ${userScore} and the computer's total score is ${compScore}. You win the whole game!!!`);
-
+        resultOutput.textContent = `Your total score is ${userScore} and the computer's total score is ${compScore}. You win the whole game!!!`;
     } else {
-        score = console.log (`Your total score is ${userScore} and the computer's total score is ${compScore}. The computer wins :(`);
-
+        resultOutput.textContent = `Your total score is ${userScore} and the computer's total score is ${compScore}. The computer wins :(`;
     }
 }
 
